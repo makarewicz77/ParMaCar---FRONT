43e3585e62a91ef803ac433bc8a6f2b780630530
makarewicz77 <makarewicz77@gmail.com>
2020-08-28 20:07:14 +0200

    stworzony szkielet komponentu kategorii

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import Test from './components/test'
+import Categories from './components/categories'
 import 'antd/dist/antd.css'
 
 function App() {
   return (
     <div className="App">
       <Test test="string" test2={2}></Test>
+
+      <div className="cat_list_div">
+        <Categories></Categories>
+      </div>
+
     </div>
   );
 }
