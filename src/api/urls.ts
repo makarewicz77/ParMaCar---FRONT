af8cd338fd3849427dc796732dd2a6f1a4df0444
Maciek <parfienczyk98@gmail.com>
2020-09-13 20:39:31 +0200

    Sending valid data, to server without setting to homepage

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -3,3 +3,4 @@ import { environment } from "../environment";
 export const baseLoginUrl = `${environment.apiUrl}api/auth/login`;
 export const baseUserUrl = `${environment.apiUrl}api/auth/user`;
 export const baseLogoutUrl = `${environment.apiUrl}api/auth/logout`;
+export const baseRegisterUrl = `${environment.apiUrl}api/auth/register`;
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,14 +1,18 @@
-export class User{
-    id:number;
-    name?:string;
-    username: string;
-    email: string;
-    password:string;
-    constructor(obj: User){
-        this.id = obj.id;
-        this.name = obj.name;
-        this.email = obj.email;
-        this.username = obj.username;
-        this.password = obj.password;
-    }
+export class User {
+  id: number;
+  name?: string;
+  first_name?: string;
+  last_name?: string;
+  username: string;
+  email: string;
+  password: string;
+  constructor(obj: User) {
+    this.id = obj.id;
+    this.name = obj.name;
+    this.first_name = obj.first_name;
+    this.last_name = obj.last_name;
+    this.email = obj.email;
+    this.username = obj.username;
+    this.password = obj.password;
+  }
 }
